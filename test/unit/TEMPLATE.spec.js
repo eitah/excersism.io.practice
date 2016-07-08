e2aa34b2a0af7c28ccb9755b6e867b3aba2e86f6
Daryl McGhee <mcgheedaryl0331@gmail.com>
2016-07-08 07:53:01 -0500

    checkin for morning

---
+++ test/unit/TEMPLATE.spec.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-var, quotes, semi-spacing, no-unused-expressions,
+prefer-arrow-callback, space-before-function-paren, padded-blocks,
+comma-spacing */
+require('../../server/models/bob.js');
